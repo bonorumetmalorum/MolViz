12f7d2d0c397617b6d6644ac7b2731a8d4d25df9
Govind Venkatesh <venkateshgovind1@gmail.com>
2020-06-25 23:46:52 +0200

    Changed Atom to struct datatype

--- Source/MolViz/AAtom.cpp
+++ Source/MolViz/AAtom.cpp
@@ -3,10 +3,19 @@
 
 #include "AAtom.h"
 
-AAtom::AAtom()
+FAtomData::FAtomData():
+	Snum(0),
+	Alt(0),
+	Chain(0),
+	Resnum(0),
+	Insertion_residue_code(0),
+	Occupancy(0),
+	TempFactor(0),
+	Element{}
 {
+	
 }
 
-AAtom::~AAtom()
+FAtomData::~FAtomData()
 {
 }
--- Source/MolViz/AAtom.h
+++ Source/MolViz/AAtom.h
@@ -7,42 +7,50 @@
 #include "AAtom.generated.h"
 
 /**
- * Represents 1 atom
+ * Represents 1 atom and all of its associated data
  */
-UCLASS()
-class MOLVIZ_API AAtom : public AInfo
+USTRUCT(BlueprintType)
+struct FAtomData
 {
 	GENERATED_BODY()
-public:
-	AAtom();
-	~AAtom();
 
-private:
+	FAtomData();
+	~FAtomData();
 	
-	uint8_t nameindex;              ///< atom name string index
-	uint8_t typeindex;              ///< atom type string index
-	uint32_t uniq_resid;               ///< unique resid, since there can be dups
-	uint32_t bondTo[12L];     ///< list of atoms to which this atom is bonded - Max is 12
-	signed char bonds;            ///< how many bonds this atom has
-	signed char atomicnumber;     ///< element atomic number
-	signed char altlocindex;      ///< alternate location identifier index
-	char insertionstr[2];         ///< for insertion codes (padded to 2 chars)
+	//uint8_t nameindex;              ///< atom name string index
+	//uint8_t typeindex;              ///< atom type string index
+	//uint32_t uniq_resid;               ///< unique resid, since there can be dups
+	//uint32_t bondTo[12L];     ///< list of atoms to which this atom is bonded - Max is 12
+	//signed char bonds;            ///< how many bonds this atom has
+	//signed char atomicnumber;     ///< element atomic number
+	//signed char altlocindex;      ///< alternate location identifier index
+	//char insertionstr[2];         ///< for insertion codes (padded to 2 chars)
 
-	// items which could potentially be moved into other data structures 
-	// to save memory, but are presently kept here for extra simplicity or speed
-	uint8_t chainindex;             ///< chain identifier
-	uint8_t segnameindex;           ///< atom segment name string index
-	uint32_t resid;                    ///< resid from original file
-	uint8_t resnameindex;           ///< atom residue name string index
+	//// items which could potentially be moved into other data structures 
+	//// to save memory, but are presently kept here for extra simplicity or speed
+	//uint8_t chainindex;             ///< chain identifier
+	//uint8_t segnameindex;           ///< atom segment name string index
+	//uint32_t resid;                    ///< resid from original file
+	//uint8_t resnameindex;           ///< atom residue name string index
 
-	// ATOMNORMAL, ATOMPROTEINBACK, ATOMNUCLEICBACK, ATOMHYDROGEN
-	// XXX this should be converted to an unsigned bit-field to save memory
-	signed char atomType;         ///< is this atom part of the backbone?
+	//// ATOMNORMAL, ATOMPROTEINBACK, ATOMNUCLEICBACK, ATOMHYDROGEN
+	//// XXX this should be converted to an unsigned bit-field to save memory
+	//signed char atomType;         ///< is this atom part of the backbone?
 
-	/// used to tell me if this atom is part of some larger complete structure
-	// RESNOTHING, RESPROTEIN, RESNUCLEIC, RESWATERS
-	// XXX this should be converted to an unsigned bit-field to save memory
-	signed char residueType;      ///< is this part of a larger component?
-								  ///< for instance, is this CG atom in an 
-								  ///< amino acid of some sort?
+	///// used to tell me if this atom is part of some larger complete structure
+	//// RESNOTHING, RESPROTEIN, RESNUCLEIC, RESWATERS
+	//// XXX this should be converted to an unsigned bit-field to save memory
+	//signed char residueType;      ///< is this part of a larger component?
+	//							  ///< for instance, is this CG atom in an 
+	//							  ///< amino acid of some sort?
+	//
+	int32 Snum;
+	uint8 Alt;
+	uint8 Chain;
+	int32 Resnum;
+	uint8 Insertion_residue_code;
+	FVector position;
+	float Occupancy;
+	float TempFactor;
+	uint8 Element[3];
 };
